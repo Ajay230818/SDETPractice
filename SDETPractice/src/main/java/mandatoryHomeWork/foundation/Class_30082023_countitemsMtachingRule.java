aa63f66d7d1aae88bf29bedd50bd2b41a85c9156
Ajay230818 <parthivajay.vt@gmail.com>
2023-08-30 09:06:32 +0530

    30-08-2023 2 nf program mentor given

--- SDETPractice/src/main/java/mandatoryHomeWork/foundation/Class_30082023_countitemsMtachingRule.java
+++ SDETPractice/src/main/java/mandatoryHomeWork/foundation/Class_30082023_countitemsMtachingRule.java
@@ -6,19 +6,62 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class Class_30082023_countitemsMtachingRule {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	@Test
+	public void test1() {
 		List<List<String>> items = new ArrayList<>();
-        items.add(0, Arrays.asList("phone","blue","pixel"));
-        items.add(1, Arrays.asList("computer","silver","lenovo"));
-        items.add(2, Arrays.asList("phone","gold","iphone"));
-        
-       System.out.println(items);
-       for (int i = 0; i < items.size(); i++) {
-		//System.out.println(items[i]);
+		items.add(0, Arrays.asList("phone", "blue", "pixel"));
+		items.add(1, Arrays.asList("computer", "silver", "lenovo"));
+		items.add(2, Arrays.asList("phone", "gold", "iphone"));
+		Assert.assertEquals(1, countmatchrule(items, "color", "silver"));
 	}
+	
+	@Test
+	public void test2() {
+		List<List<String>> items = new ArrayList<>();
+		items.add(0, Arrays.asList("phone", "blue", "pixel"));
+		items.add(1, Arrays.asList("computer", "silver", "lenovo"));
+		items.add(2, Arrays.asList("phone", "gold", "iphone"));
+		Assert.assertEquals(2, countmatchrule(items, "type", "phone"));
+	}
+	
+	@Test
+	public void test3() {
+		List<List<String>> items = new ArrayList<>();
+		items.add(0, Arrays.asList("phone", "blue", "pixel"));
+		items.add(1, Arrays.asList("computer", "silver", "lenovo"));
+		items.add(2, Arrays.asList("phone", "gold", "iphone"));
+		Assert.assertEquals(1, countmatchrule(items, "name", "lenovo"));
 	}
 
-}
+	public static int countmatchrule(List<List<String>> items,String ruleKey,String ruleValue) {
+		int count = 0;
+		int rule =0;
+		switch (ruleKey) {
+		// Case statements
+		case "type":
+			rule = 0;
+			break;
+		case "color":
+			rule = 1;
+			break;
+		case "name":
+			rule = 2;
+		}
+
+		for (int i = 0; i < items.size(); i++) {
+			for (int j = 0; j < items.get(i).size(); j++) {
+				if (items.get(i).get(rule) == ruleValue && i==j) {
+					count++;
+				}
+			}
+		}
+		return count;
+    }
+	}
+
