723bb2c5aea0c4f9eda208f9271bb3922c69f93b
Ajay230818 <parthivajay.vt@gmail.com>
2023-09-13 06:44:53 +0530

    QChallenge76 12-09-2023

---
+++ SDETPractice/src/main/java/mandatoryHomeWork/foundation/Qchallenge76_NumberOflinestoWriteString.java
@@ -0,0 +1,63 @@
+package mandatoryHomeWork.foundation;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class Qchallenge76_NumberOflinestoWriteString {
+	
+	@Test
+	public void test1() {
+		int [] actual=new int [2];
+		actual[0]=3;
+		actual[1]=60;
+		Assert.assertArrayEquals(actual, numberOfLines(new int [] {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "abcdefghijklmnopqrstuvwxyz"));
+	}
+	
+	@Test
+	public void test2() {
+		int [] actual=new int [2];
+		actual[0]=2;
+		actual[1]=4;
+		Assert.assertArrayEquals(actual, numberOfLines(new int [] {4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "bbbcccdddaaa"));
+	}
+
+	public static int[] numberOfLines(int[] widths, String s) {
+		int count=0;
+		int pixels=100;
+		char [] ch= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		int [] out=new int[2];
+		
+		HashMap<Character, Integer> map=new HashMap<Character, Integer>();
+		for (int i = 0; i < widths.length; i++) {
+			for (int j = 0; j < ch.length; j++) {
+				map.put(ch[i], widths[i]);
+			}
+		}
+		
+		for (int i = 0; i < s.length(); i++) {
+				pixels-=map.get(s.charAt(i));
+				if(pixels==0) {
+					count++;
+					pixels=100;
+				}else if(pixels<0) {
+					count++;
+					i--;
+					pixels=100;
+				}
+		}
+		if(100-pixels>0 && 100-pixels<100) {
+			count++;
+		}
+		out[0]=count;
+		if(pixels<100) {
+		out[1] = 100 - pixels;}
+		else {
+			out[1]=100;
+		}
+		return out;
+    }
+	}
